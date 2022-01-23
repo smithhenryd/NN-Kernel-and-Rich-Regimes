e6ba722510976e3db71b562b3b3abd9923d6db24
smithhenryd <henry.smith@yale.edu>
2022-01-23 14:49:54 -0500

    Fix bias term in nonlinear model

--- Code/Linearized_Model/linearized_model.py
+++ Code/Linearized_Model/linearized_model.py
@@ -1,26 +1,37 @@
 import tensorflow as tf
-from ..Linear_NN.model import Linear_Regression
+from Linear_NN.model import Linear_Regression
 
 class Linearized_Model(tf.keras.Model):
     """
-    The linearized version of a preexisting TensorFlow model, as discussed in
+    The linearized version of a preexisting, D-homogeneous TensorFlow model, as discussed in
     Chizat et al. 2018 as well as in Woodworth et al. 2020
     """
 
-    def __init__(self, model, **kwargs):
+    def __init__(self, model, alpha=1, hom=1, **kwargs):
         """
         Initializes the network, which is simply 'model' linearized around its initialization 'w0'
 
-        model: a tensorflow.keras.Model object, representing the [nonlinear] model
+        model: a tensorflow.keras.Model object, representing the [nonlinear], D-homogeneous model
         for which one would like to compute the linearized model
         NOTE: the model must have current weight vector equal to its initialization (i.e. it should not be trained yet);
         if this is an issue, one should use copy.deepcopy or tf.keras.models.clone_model
+
+        alpha: a nonnegative float, representing the scale with which 'model' was initialized
+        hom: a positive integer, the degree of homogeneity of 'model'
         """
 
         super(Linearized_Model, self).__init__(**kwargs)
 
-        self.linearized_layer_1 = Linearized_Layer(model, **kwargs)
-    
+        self.linearized_layer_1 = Linearized_Layer(model, alpha, hom, **kwargs)
+
+    def _compute_homogeneity_degree(self, model):
+        """
+        Computes the degree of homogeneity for the [nonlinear] model
+
+        model: a tensorflow.keras.Model object
+        """
+        raise NotImplementedError
+
     def call(self, inputs) -> tf.Tensor:
         """
         Evaluates the linearized model at the specified inputs
@@ -38,13 +49,15 @@ class Linearized_Layer(tf.keras.layers.Layer):
     The single layer of the linearized network
     """
 
-    def __init__(self, model, **kwargs):
+    def __init__(self, model, alpha, hom, **kwargs):
         """
         Initializes the linearized layer
         """
 
         super(Linearized_Layer, self).__init__(**kwargs)
         
+        # Compute the factor by which the bias term is scaled
+        self.bias_scale = alpha**hom
 
         # Initialize the weight vector [that which we will train]
         self.w = tf.Variable(initial_value=tf.reshape(model.trainable_weights, [-1, 1]), trainable=True)
@@ -84,7 +97,7 @@ class Linearized_Layer(tf.keras.layers.Layer):
         # As well as the gradient evaluated at the inputs
         grads = self._compute_gradient(inputs, model)
 
-        return bias + tf.matmul(grads ,tf.reshape(self.w - tf.reshape(model.trainable_weights, [-1, 1]), [-1, 1]))
+        return self.bias_scale*bias + tf.matmul(grads ,tf.reshape(self.w - tf.reshape(model.trainable_weights, [-1, 1]), [-1, 1]))
 
 if __name__ == "__main__":
 
@@ -96,7 +109,8 @@ if __name__ == "__main__":
     model = Linear_Regression(w0)
 
     # Instantiate the linearized model
-    linearized_model = Linearized_Model(model)
+    # The linear regression model is 2-homogeneous
+    linearized_model = Linearized_Model(model, alpha=1, hom=2)
     
     # And evaluate it at N input vectors
     N = 5
